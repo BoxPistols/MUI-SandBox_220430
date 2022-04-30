bd9287369c833b93dcf2ccbdc2fc6ab0059541ac
aito <BoxPistols@users.noreply.github.com>
2022-05-01 01:25:45 +0900

    Dev Setting React18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import StyleWrap from './Style'
 import { StyledEngineProvider } from '@mui/material/styles'
 import Form from './Form'
 import CustomForm from './CustomForm'
@@ -7,29 +8,49 @@ import Demo from './Typography'
 import CustomTypo from './CustomTypo'
 
 // before
-ReactDOM.render(
-  <StyledEngineProvider injectFirst>
-    <CustomForm />
-    <hr />
-    <Form />
-    <hr />
-    <CustomTypo />
-    <hr />
-    <Demo/>
-  </StyledEngineProvider>,
-  document.querySelector('#root')
-)
+// ReactDOM.render(
+//   <StyledEngineProvider injectFirst>
+//     <StyleWrap>
+//       <CustomForm />
+//       <hr />
+//       <Form />
+//       <hr />
+//       <CustomTypo />
+//       <hr />
+//       <Demo />
+//     </StyleWrap>
+//   </StyledEngineProvider>,
+//   document.querySelector('#root')
+// )
 
 /**
  * Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot
  */
 // After
 
-// import { createRoot } from 'react-dom/client'
-// const container = document.getElementById('root')
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+
+if ( container ) {
+  const root = createRoot(container);
+  root.render(  <StyledEngineProvider injectFirst>
+    <StyleWrap>
+      <CustomForm />
+      <hr />
+      <Form />
+      <hr />
+      <CustomTypo />
+      <hr />
+      <Demo />
+    </StyleWrap>
+  </StyledEngineProvider>);
+}
+
 // const root = createRoot(container) // createRoot(container!) if you use TypeScript
 // root.render(
 //   <StyledEngineProvider injectFirst>
 //     <Demo />
 //   </StyledEngineProvider>
 // )
+
+
