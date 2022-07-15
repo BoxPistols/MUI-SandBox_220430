4bc1fb2f094e5f52b10d5c852936706256f13046
aito <BoxPistols@users.noreply.github.com>
2022-07-15 20:39:23 +0900

    Create Files

---
+++ src/components/Button/ButtonGroup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const ButtonGroup = () => {
+  return (
+    <>
+      <section>
+        <h2>ButtonGroup</h2>
+        <p>paragraph</p>
+      </section>
+    </>
+  );
+};
---
+++ src/components/Button/Submit.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Submit = () => {
+  return (
+    <>
+      <section>
+        <h2>Submit</h2>
+        <p>paragraph</p>
+      </section>
+    </>
+  );
+};
